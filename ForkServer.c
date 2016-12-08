6c2983d3d177cfae80f765b817378b64426cc927
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2016-12-07 19:47:01 -0500

    The server now can get the client address and print it

--- ForkServer.c
+++ ForkServer.c
@@ -11,7 +11,9 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-void dostuff(int); /* function prototype */
+void dostuff(int);
+char* getAddress(unsigned int cli_addr); /* function prototype */
+
 void error(char *msg)
 {
     perror(msg);
@@ -20,42 +22,53 @@ void error(char *msg)
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno, clilen, pid;
-     struct sockaddr_in serv_addr, cli_addr;
+  if (argc < 2) {
+    fprintf(stderr,"ERROR, no port provided\n");
+    exit(1);
+  }
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) 
+    error("ERROR opening socket");
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  portno = atoi(argv[1]);
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(portno);
+  if (bind(sockfd, (struct sockaddr *) &serv_addr,
+    sizeof(serv_addr)) < 0) 
+    error("ERROR on binding");
+  listen(sockfd,5);
+  clilen = sizeof(cli_addr);
+  while (1) {
+    newsockfd = accept(sockfd, 
+         (struct sockaddr *) &cli_addr, &clilen);
+    printf("%s", getAddress(cli_addr.sin_addr.s_addr));
+    if (newsockfd < 0) 
+       error("ERROR on accept");
+    pid = fork();
+    if (pid < 0)
+       error("ERROR on fork");
+    if (pid == 0)  {
+       close(sockfd);
+       dostuff(newsockfd);
+       exit(0);
+    }
+    else close(newsockfd);
+  } /* end of while */
+  return 0; /* we never get here */
+}
 
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
-        error("ERROR opening socket");
-     memset(&serv_addr, 0, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     while (1) {
-         newsockfd = accept(sockfd, 
-               (struct sockaddr *) &cli_addr, &clilen);
-         if (newsockfd < 0) 
-             error("ERROR on accept");
-         pid = fork();
-         if (pid < 0)
-             error("ERROR on fork");
-         if (pid == 0)  {
-             close(sockfd);
-             dostuff(newsockfd);
-             exit(0);
-         }
-         else close(newsockfd);
-     } /* end of while */
-     return 0; /* we never get here */
+/*
+  This function returns the pointer to the string representation of an IP address encoded in an unsigned integer
+*/
+char* getAddress(unsigned int addr) {
+  char* result[15];
+  unsigned int a0 = addr & 0xFF;
+  unsigned int a1 = (addr >> 8) & 0xFF;
+  unsigned int a2 = (addr >> 16) & 0xFF;
+  unsigned int a3 = (addr >> 24) & 0xFF;
+  snprintf(result, 15, "%d.%d.%d.%d", a0, a1, a2, a3);
+  return result;
 }
 
 /******** DOSTUFF() *********************
