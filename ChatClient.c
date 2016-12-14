daec7ec50fb1fa5fb6bc6c22a25f6e3130281d40
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2016-12-14 00:00:29 -0500

    Squash merge with server and client

---
+++ ChatClient.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <string.h>
+#include <pthread.h>
+
+#define SERVADDR_LEN 100
+#define BUF_SIZE 256
+
+void error(char *msg)
+{
+  perror(msg);
+  exit(0);
+}
+void * receiveMessage(void * socket) {
+  int sockfd, ret;
+  int k = 0;
+  char buffer[BUF_SIZE];
+  sockfd = (int) socket;
+  char server_name[BUF_SIZE];
+
+  memset(buffer, 0, BUF_SIZE);
+  while ((ret = read(sockfd, buffer, BUF_SIZE)) > 0) {
+    if (k == 0) {
+      strcpy(server_name, buffer);
+      k = 1;
+      int i;
+      for(i = 0; i < BUF_SIZE; i++){
+      if (server_name[i] == '\n') break;
+      }
+      bzero(&server_name[i], BUF_SIZE - i);
+      printf("(%s)\n", server_name);
+    }
+    else {
+      printf("<%s>%s", server_name, buffer);
+    }
+    bzero(buffer, BUF_SIZE);  
+  }
+  if (ret < 0) 
+    printf("Error receiving data!\n");
+  else{
+    printf("Closing connection\n");
+  }
+  close(sockfd);
+  exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+  int sockfd, portno, n, ret;
+  struct sockaddr_in serv_addr;
+  struct hostent *server;
+  char buffer[BUF_SIZE];
+  if (argc < 3) {
+    fprintf(stderr,"usage %s hostname port\n", argv[0]);
+    exit(0);
+  }
+
+  printf("Provide user name: ");
+  char name[BUF_SIZE];
+  scanf("%s", name);
+
+  portno = atoi(argv[2]);
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) 
+    error("ERROR opening socket");
+  server = gethostbyname(argv[1]);
+  if (server == NULL) {
+    fprintf(stderr,"ERROR, no such host\n");
+    exit(0);
+  }
+
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  bcopy((char *)server->h_addr, 
+      (char *)&serv_addr.sin_addr.s_addr,
+      server->h_length);
+  serv_addr.sin_port = htons(portno);
+  printf("Waiting for connection...\n");
+  if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
+    error("ERROR connecting");
+  char serverAddr[SERVADDR_LEN];
+  inet_ntop(AF_INET, &(serv_addr.sin_addr), serverAddr, SERVADDR_LEN);
+  printf("Connection accepted from %s...", serverAddr);
+  write(sockfd, name, strlen(name));
+  pthread_t rThread;
+  if (ret = pthread_create(&rThread, NULL, receiveMessage, (void *) sockfd)) {
+    printf("ERROR: Return Code from pthread_create() is %d\n", ret);
+    error("ERROR creating thread");
+  }
+  bzero(buffer,BUF_SIZE);
+  while(1)
+  {
+    bzero(buffer,BUF_SIZE);
+    fgets(buffer,BUF_SIZE,stdin);
+    n = write(sockfd,buffer,strlen(buffer));
+    if (n < 0) 
+        error("ERROR writing to socket");
+    char EXIT[BUF_SIZE];
+    bzero(EXIT, BUF_SIZE);
+    strcpy(EXIT, "exit");
+
+    int j;
+    for(j = 0; j < BUF_SIZE; j++)
+    {
+      if(buffer[j] == '\n') break;
+    }
+    bzero(&buffer[j], BUF_SIZE - j);
+    if(strcoll(EXIT, buffer) == 0) break;
+    bzero(buffer,BUF_SIZE);
+  }
+  printf("Closing connection\n");
+  close(sockfd);
+  return 0;
+}
--- ForkServer.c
+++ ForkServer.c
@@ -11,23 +11,59 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-void dostuff(int); /* function prototype */
+int currentPid;
+int mainPid;
+int mainPidSet = 0;
+int writePid;
+int writePidSet = 0;
+int currentSocket;
+
+/* function prototype */
+void getAddress(unsigned int addr, char* result);
+void getClientUsername(int sock, char* serverUsername, char* clientUsername);
+int isExit(char* input);
+void readSocket (int sock, char* addr, char* username, int mainPid);
+void writeSocket (int sock, int readPid);
+
 void error(char *msg)
 {
   perror(msg);
   exit(1);
 }
 
+void term(int signum) {
+  close(currentSocket); // Don't really mind if this is closed more than once
+  if (writePidSet) {
+    kill(writePid, SIGTERM);
+  }
+  if (mainPidSet) {
+    kill(mainPid, SIGTERM);
+  }
+  exit(0);
+}
+
 int main(int argc, char *argv[])
 {
-  int sockfd, newsockfd, portno, clilen, pid;
+  int sockfd, portno, clilen, pid, running;
   struct sockaddr_in serv_addr, cli_addr;
+  char* serverUsername[32];
+  char clientUsername[32];
+  char clientAddress[15];
+  struct sigaction action;
+  memset(&action, 0, sizeof(struct sigaction));
+  action.sa_handler = term;
+  sigaction(SIGTERM, &action, NULL);
 
   if (argc < 2) {
     fprintf(stderr,"ERROR, no port provided\n");
     exit(1);
   }
+
+  printf("%s", "Provide user name: ");
+  fgets(serverUsername, 32, stdin);
+
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  currentSocket = sockfd;
   if (sockfd < 0) 
     error("ERROR opening socket");
   memset(&serv_addr, 0, sizeof(serv_addr));
@@ -36,42 +72,123 @@ int main(int argc, char *argv[])
   serv_addr.sin_addr.s_addr = INADDR_ANY;
   serv_addr.sin_port = htons(portno);
   if (bind(sockfd, (struct sockaddr *) &serv_addr,
-    sizeof(serv_addr)) < 0) 
+            sizeof(serv_addr)) < 0) 
     error("ERROR on binding");
   listen(sockfd,5);
+
+  printf("%s", "Waiting for connection ...\n");
+
   clilen = sizeof(cli_addr);
+
+  currentPid = getpid();
+
   while (1) {
-    newsockfd = accept(sockfd, 
-         (struct sockaddr *) &cli_addr, &clilen);
-    if (newsockfd < 0) 
-       error("ERROR on accept");
+    int newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    if (newsockfd < 0)
+      error("ERROR on accept");
+    getAddress(cli_addr.sin_addr.s_addr, clientAddress);
     pid = fork();
     if (pid < 0)
        error("ERROR on fork");
-    if (pid == 0)  {
-       close(sockfd);
-       dostuff(newsockfd);
-       exit(0);
+    if ((pid == 0) & (pid != currentPid)) {
+      getClientUsername(newsockfd, serverUsername, clientUsername);
+      readSocket(newsockfd, clientAddress, clientUsername, currentPid);
     }
-    else close(newsockfd);
   } /* end of while */
+  kill(pid, SIGTERM);
   return 0; /* we never get here */
 }
 
-/******** DOSTUFF() *********************
- There is a separate instance of this function 
- for each connection.  It handles all communication
- once a connnection has been established.
- *****************************************/
-void dostuff (int sock)
+/*
+  This function returns the pointer to the string representation of an IP address encoded in an unsigned integer
+*/
+void getAddress(unsigned int addr, char* result) {
+  unsigned int a0 = addr & 0xFF;
+  unsigned int a1 = (addr >> 8) & 0xFF;
+  unsigned int a2 = (addr >> 16) & 0xFF;
+  unsigned int a3 = (addr >> 24) & 0xFF;
+  snprintf(result, 15, "%d.%d.%d.%d", a0, a1, a2, a3);
+}
+
+void getClientUsername(int sock, char* serverUsername, char* clientUsername) {
+  int n;
+  memset(clientUsername, 0, 32);
+  n = read(sock, clientUsername, 32);
+
+  for (int i = 0; i < 32; i++) {
+    if (clientUsername[i] == '\n' | clientUsername[i] == '\r') {
+      clientUsername[i] = 0;
+    }
+  }
+
+  if (n < 0) error("ERROR reading from socket");
+  n = write(sock, serverUsername, sizeof serverUsername);
+  if (n < 0) error("ERROR writing to socket");
+}
+
+int isExit(char* input) {
+  int r;
+  r = strcmp(input, (char *) "exit") & strcmp(input, (char *) "exit\n");
+  return (r == 0);
+}
+
+void killProcess(int sock, int mainPid, int readPid, int writePid) {
+  close(sock);
+}
+
+void readSocket (int sock, char* addr, char* username, int mainPid)
 {
+  int n, pid;
+  char input[256];
+
+  currentPid = getpid();
+  currentSocket = sock;
+  mainPid = mainPid;
+  mainPidSet = 1;
+  printf("Connection established with %s (%s)\n", addr, username);
+
+  memset(input, 0, 256);
+  pid = fork();
+  writePid = pid;
+  writePidSet = 1;
+  if (pid < 0)
+     error("ERROR on fork");
+  if ((pid == 0) & (pid != currentPid)) {
+    writeSocket(sock, currentPid);
+  } else {
+    while (1) {
+      n = read(sock, input, 255);
+      if (n < 0) error("ERROR reading from socket");
+      if (isExit(input)) {
+        break;
+      }
+      printf("<%s>%s", username, input);
+      memset(input, 0, 256);
+    }
+    printf("%s", "Closing connection ...");
+  }
+  kill(currentPid, SIGTERM);
+}
+
+void writeSocket (int sock, int readPid) {
   int n;
   char buffer[256];
-    
   memset(buffer, 0, 256);
-  n = read(sock,buffer,255);
-  if (n < 0) error("ERROR reading from socket");
-  printf("Here is the message: %s\n",buffer);
-  n = write(sock,"I got your message",18);
-  if (n < 0) error("ERROR writing to socket");
+
+  currentPid = getpid();
+  mainPidSet = 0;
+  writePidSet = 0;
+
+  while (1) {
+    fgets(buffer, 256, stdin);
+    if (isExit(buffer)) {
+      write(sock, "exit\n", 5);
+      break;
+    }
+    n = write(sock, buffer, sizeof buffer);
+    if (n < 0) error("ERROR writing to socket");
+    printf("<you>%s", buffer);
+  }
+  printf("%s", "Closing connection ...");
+  kill(readPid, SIGTERM);
 }
